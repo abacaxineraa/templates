4ec26e946f719a9ba44db7945d3326bc787103a1
Ana Torres <anasofitorresbe@gmail.com>
2024-09-13 01:18:15 -0400

    changed to poetry2nix

--- python/flake.nix
+++ python/flake.nix
@@ -1,26 +1,39 @@
 {
-  description = "A Nix-flake-based Python development environment";
+  description = "PyNoter";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # pynoter.url = "github:maxrousseau/pynoter";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
       });
     in
-    {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          venvDir = ".venv";
-          packages = with pkgs; [ python311 ] ++
-            (with pkgs.python311Packages; [
-              pip
-              venvShellHook
-              numpy
-            ]);
-        };
-      });
-    };
+      {
+        devShells = forEachSupportedSystem ({ pkgs }: {
+          default = pkgs.mkShell {
+            venvDir = ".venv";            
+            packages = with pkgs; [ python311 poetry ] ++
+                                  (with pkgs.python311Packages; [
+                                    pip
+                                    venvShellHook
+                                    numpy
+                                  ]);
+          };
+          poetry = pkgs.mkShell {
+            packages = [ pkgs.poetry ];
+          };
+        });
+      };
 }
+
+  # poetry init, lock, add [lib]
